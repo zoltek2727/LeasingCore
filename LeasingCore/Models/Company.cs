5d745cebe7aea171170df5455433d534da1195d6
Sebastian <zoltek2727@wp.pl>
2018-11-09 18:47:06 +0100

    Two models and migration.

---
+++ LeasingCore/Models/Company.cs
@@ -0,0 +1,8 @@
+﻿namespace LeasingCore.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ LeasingCore/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace LeasingCore.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+    }
+}
